4a55d337e5068f27806adbbf0b82bb065f6669fa
SassanidBytes <sassanidbytes@gmail.com>
2026-05-21 14:55:14 +0200

    Add type annotations to memoization.py

---
+++ sympy/utilities/memoization.py
@@ -0,0 +1,77 @@
+from __future__ import annotations
+from functools import wraps
+from mimetypes import init
+from typing import Callable, Any 
+
+def recurrence_memo(initial: list[Any]) -> Callable:
+    """
+    Memo decorator for sequences defined by recurrence
+
+    Examples
+    ========
+
+    >>> from sympy.utilities.memoization import recurrence_memo
+    >>> @recurrence_memo([1]) # 0! = 1
+    ... def factorial(n, prev):
+    ...     return n * prev[-1]
+    >>> factorial(4)
+    24
+    >>> factorial(3) # use cache values
+    6
+    >>> factorial.cache_length() # cache length can be obtained
+    5
+    >>> factorial.fetch_item(slice(2, 4))
+    [2, 6]
+
+    """
+    cache = initial
+
+    def decorator(f: Callable) -> Callable:
+        @wraps(f)
+        def g(n: int)-> Any:
+            L = len(cache)
+            if n < L:
+                return cache[n]
+            for i in range(L, n + 1):
+                cache.append(f(i, cache))
+            return cache[-1]
+        g.cache_length = lambda: len(cache)
+        g.fetch_item = lambda x: cache[x]
+        return g
+    return decorator
+
+
+def assoc_recurrence_memo(base_seq: Callable) -> Callable:
+    """
+    Memo decorator for associated sequences defined by recurrence starting from base
+
+    base_seq(n) -- callable to get base sequence elements
+
+    XXX works only for Pn0 = base_seq(0) cases
+    XXX works only for m <= n cases
+    """
+
+    cache: list[list[Any]] = []
+
+    def decorator(f: Callable) -> Callable:
+        @wraps(f)
+        def g(n: int, m:int)-> Any:
+            L = len(cache)
+            if n < L:
+                return cache[n][m]
+
+            for i in range(L, n + 1):
+               
+                F_i0 = base_seq(i)
+                F_i_cache = [F_i0]
+                cache.append(F_i_cache)
+
+
+                for j in range(1, i + 1):
+                    F_ij = f(i, j, cache)
+                    F_i_cache.append(F_ij)
+
+            return cache[n][m]
+
+        return g
+    return decorator
